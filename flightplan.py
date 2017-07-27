c89193123abcc2538bbd3ac0baf4a435f01e8a4e
lewisvtu <lewislight2012@googlemail.com>
2017-07-27 09:39:32 +0100

    Discovered a nice flightplan

--- flightplan.py
+++ flightplan.py
@@ -9,53 +9,53 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import spline
 
 h = 0.6777
-# SQL = """
-#     SELECT
-#         DES.GalaxyID,
-#         PROG.SnapNum,
-#         PROG.Mass,
-#         PROG.CentreOfPotential_x,
-#         PROG.CentreOfPotential_y,
-#         PROG.CentreOfPotential_z,
-#         PROG.Redshift
-#     FROM
-#         RefL0100N1504_Subhalo as PROG with(forceseek),
-#         RefL0100N1504_Subhalo as DES,
-#         RefL0100N1504_Aperture as AP
-#     WHERE
-#         DES.SnapNum = 28 and
-#         DES.MassType_Star > 1.0e9 and
-#         DES.MassType_DM > 5.0e10 and
-#         PROG.GalaxyID between DES.GalaxyID and DES.TopLeafID and
-#         AP.ApertureSize = 30 and
-#         AP.GalaxyID = DES.GalaxyID and
-#         AP.Mass_Star > 1.0e9
-#     ORDER BY
-#         PROG.GalaxyID,
-#         PROG.SnapNum
-# """
+SQL = """
+    SELECT
+        DES.GalaxyID,
+        PROG.SnapNum,
+        PROG.Mass,
+        PROG.CentreOfPotential_x,
+        PROG.CentreOfPotential_y,
+        PROG.CentreOfPotential_z,
+        PROG.Redshift
+    FROM
+        RefL0100N1504_Subhalo as PROG with(forceseek),
+        RefL0100N1504_Subhalo as DES,
+        RefL0100N1504_Aperture as AP
+    WHERE
+        DES.SnapNum = 28 and
+        DES.MassType_Star > 1.0e9 and
+        DES.MassType_DM > 5.0e10 and
+        PROG.GalaxyID between DES.GalaxyID and DES.TopLeafID and
+        AP.ApertureSize = 30 and
+        AP.GalaxyID = DES.GalaxyID and
+        AP.Mass_Star > 1.0e9
+    ORDER BY
+        PROG.GalaxyID,
+        PROG.SnapNum
+"""
 
-# # Grabs new data from db based on sql. If file name already exists, it loads that data instead
+# Grabs new data from db based on sql. If file name already exists, it loads that data instead
 
-# filename = "FollowProgs2.p"
+filename = "FollowProgs2.p"
 
-# raw_dbs = dbsPull(SQL, filename)
+raw_dbs = dbsPull(SQL, filename)
 
-# shelf.push(raw_dbs, "followup2")
+shelf.push(raw_dbs, "followup2")
 
-# dbs_data = shelf.pull("followup2")
+dbs_data = shelf.pull("followup2")
 
-# interesting_ids = {
-#     13660659: 28,
-#     13793733: 28,
-#     13722615: 28
+interesting_ids = {
+    13660659: 28,
+    13793733: 28,
+    13722615: 28
 
-# }
-# gals = np.asarray([list(gal)[3:] for gal in dbs_data if gal[0] in interesting_ids.keys() and gal[1] == interesting_ids[gal[0]]])
-# # Makes all galaxies have the form [sf,x,y,z] sorted by sf
-# gals[:,-1] = 1.0 / (1.0 + gals[:,-1])
-# gals = gals[np.argsort(gals[:,3])]
-# gals = gals[:,[3,0,1,2]]
+}
+gals = np.asarray([list(gal)[3:] for gal in dbs_data if gal[0] in interesting_ids.keys() and gal[1] == interesting_ids[gal[0]]])
+# Makes all galaxies have the form [sf,x,y,z] sorted by sf
+gals[:,-1] = 1.0 / (1.0 + gals[:,-1])
+gals = gals[np.argsort(gals[:,3])]
+gals = gals[:,[3,0,1,2]]
 
 
 def circular_path(frame_nos, args):
@@ -76,7 +76,7 @@ def circular_path(frame_nos, args):
     ang_int = orbits * 2*np.pi / frames
     x_coords = target_coords[0] + dir * rad * np.sin(frame_nos * ang_int)
     y_coords = target_coords[1] + dir * rad * np.cos(frame_nos * ang_int)
-    z_coords = target_coords[2] + dir * (rad* np.sin(frame_nos * ang_int)) * 0
+    z_coords = target_coords[2] + dir * (rad* np.sin(frame_nos * ang_int)) * 0.5
     return np.transpose(np.asarray([x_coords, y_coords, z_coords]))
 
 def cam_vectors(frame_nos, target_coords, path_function, args):
@@ -134,30 +134,30 @@ def draw_graph(file_name, target_gal):
     plt.show()
 
 
-# first_frames = np.arange(10, dtype=float)
-# sec_frames = np.arange(10, dtype=float) + 20
-# third_frames = np.arange(10, dtype=float) + 40
-# first_coords = circular_path(first_frames, [gals[0,1:], 5.0, 0.5, 10, 1])
-# sec_coords = circular_path(sec_frames, [gals[1,1:], 5.0, 0.5, 10, -1])
-# third_coords = circular_path(third_frames, [gals[2,1:], 5.0, 0.5, 10, 1])
+first_frames = np.arange(10, dtype=float)
+sec_frames = np.arange(10, dtype=float) + 20
+third_frames = np.arange(10, dtype=float) + 40
+first_coords = circular_path(first_frames, [gals[0,1:], 5.0, 1, 10, -1])
+sec_coords = circular_path(sec_frames, [gals[1,1:], 5.0, 1, 10, 1])
+third_coords = circular_path(third_frames, [gals[2,1:], 5.0, 1, 10, 1])
 
-# fks = np.concatenate((first_frames, sec_frames, third_frames))
-# xks = np.concatenate((first_coords[:,0], sec_coords[:,0], third_coords[:,0]))
-# yks = np.concatenate((first_coords[:,1], sec_coords[:,1], third_coords[:,1]))
-# zks = np.concatenate((first_coords[:,2], sec_coords[:,2], third_coords[:,2]))
+fks = np.concatenate((first_frames, sec_frames, third_frames))
+xks = np.concatenate((first_coords[:,0], sec_coords[:,0], third_coords[:,0]))
+yks = np.concatenate((first_coords[:,1], sec_coords[:,1], third_coords[:,1]))
+zks = np.concatenate((first_coords[:,2], sec_coords[:,2], third_coords[:,2]))
 
-# frame_array = np.arange(50)
-# xs = spline(fks, xks, frame_array)
-# ys = spline(fks, yks, frame_array)
-# zs = spline(fks, zks, frame_array)
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection="3d")
-# ax.set_xlabel("x")
-# ax.set_ylabel("y")
-# ax.set_zlabel("z")
-# ax.plot(xs, ys, zs)
+frame_array = np.arange(50)
+xs = spline(fks, xks, frame_array)
+ys = spline(fks, yks, frame_array)
+zs = spline(fks, zks, frame_array)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+ax.plot(xs, ys, zs)
 
-# plt.show()
+plt.show()
 
 # no_of_frames = 50
 # circle_args = [gals[0,1:], 5.0, 1.0, no_of_frames]
@@ -165,4 +165,3 @@ def draw_graph(file_name, target_gal):
 # file_name = "C_path_r=5_o=1_f=50.txt"
 # gen_file(no_of_frames, gals[0], circular_path, circle_args, file_name)
 # draw_graph(file_name, gals[0,1:])
-
