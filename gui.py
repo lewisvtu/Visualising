d436fccbffb56fe5f3a4177f8ec474b08aaa6b7c
lewisvtu <xmdc36@durham.ac.uk>
2017-08-22 15:44:04 +0100

    Final???

--- gui.py
+++ gui.py
@@ -10,6 +10,8 @@ from flightplan_generator import create_flight_path
 import copy
 import storyB_V2
 import os
+from PIL import Image, ImageTk
+
 
 class MainWindow(object):
     '''Main window object for aplication'''
@@ -23,10 +25,16 @@ class MainWindow(object):
 
     def draw(self):
         self.main_frame = Frame(self.master)
+        self.img_frame = Frame(self.master)
         Button(self.main_frame, text="Quit >:(", command=self._quit).grid(row=0, column=0)
         Button(self.main_frame, text="Open Data Set", command=self.open_data).grid(row=0, column=1)
         Button(self.main_frame, text="Open Graph Viewer", command=self.open_graph).grid(row=0, column=2)
-        self.main_frame.pack()
+        photo = ImageTk.PhotoImage(Image.open(os.path.join("resource", "eagleLogo.PNG")))
+        img = Label(self.img_frame, image=photo)
+        img.image = photo
+        img.pack()
+        self.main_frame.pack(side=TOP)
+        self.img_frame.pack(side=BOTTOM)
 
     def open_graph(self):
         graph_window = Toplevel(self.master)
@@ -45,26 +53,35 @@ class GraphWindow(object):
     def __init__(self, master, fig):
         self.master = master
         self.frame = Frame(self.master)
-        Button(self.frame, text="Close Window", command=self.close_window).grid(row=0, column=0)
-        Button(self.frame, text="Draw Graph", command=self.draw_graph).grid(row=0, column=3)
-        Button(self.frame, text="Story Board", command=self.gen_story_board).grid(row=0, column=4)
-        Label(self.frame, text="Flight File Name: ").grid(row=0,column=1)
+        self.graph_f = Frame(self.master)
+        self.toolbar =Frame(self.master)
+        Button(self.toolbar, text="Close Window", command=self.close_window).grid(row=0, column=0)
+        Button(self.frame, text="Draw Graph", command=self.draw_graph).grid(row=3, column=0)
+        Button(self.frame, text="Story Board", command=self.gen_story_board).grid(row=3, column=1)
+        Label(self.frame, text="Flight File Name: ").grid(row=2,column=0)
+        Label(self.frame, text="Simulation Code: ").grid(row=1, column=0)
+        self.simname_e = Entry(master=self.frame, width=20)
+        self.simname_e.grid(row=1, column=1)
+        self.simname_e.insert(0, "RefL0025N0376")
         self.fname_e = Entry(master=self.frame, width=20)
-        self.fname_e.grid(row=0, column=2)
-        self.frame.grid()
+        self.fname_e.grid(row=2, column=1)
+        self.toolbar.pack(side=TOP)
+        self.frame.pack(side=LEFT)
+        self.graph_f.pack(side=RIGHT)
         self.set_graph(fig, des=False)
 
     def close_window(self):
         self.master.destroy()
 
     def gen_story_board(self):
+        sim_ref = self.simname_e.get()
         fname = self.fname_e.get()
         fname_root = fname[:-3]
         save_dir = os.path.join(fname_root, "images")
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         image_name_base = os.path.join(save_dir, "image_no_")
-        storyB_V2.story_board(image_name_base, fname)
+        storyB_V2.story_board(image_name_base, fname, sim_ref)
 
 
     def draw_graph(self):
@@ -73,11 +90,11 @@ class GraphWindow(object):
         self.set_graph(figure)
 
     def set_graph(self, fig, des=True):
-        self.canvas = FigureCanvasTkAgg(fig, self.frame)
+        self.canvas = FigureCanvasTkAgg(fig, self.graph_f)
         if des:
             self.canv_widget.destroy()
         self.canv_widget = self.canvas.get_tk_widget()
-        self.canv_widget.grid(row=3)
+        self.canv_widget.grid()
         ax = fig.gca()
         ax.mouse_init()
 
@@ -89,6 +106,7 @@ class DataWindow(object):
         self.data_store = data_store
         self.data_entries = []
         self.frame = Frame(self.master)
+        self.data_f = Frame(self.master)
         Button(self.frame, text="Close Window", command=self.close_window).grid(row=0, column=0)
         Button(self.frame, text="Save Data", command=self.read_entry_boxes).grid(row=0,column=1)
         Button(self.frame, text="New Gal", command=self.add_row).grid(row=0, column=2)
@@ -98,8 +116,9 @@ class DataWindow(object):
         self.fname_e.grid(row=0, column=4)
         lab_names = ["galaxy", "st fr", "en fr", "st sf", "en sf", "trg x", "y", "z", "rot ax: nx", "ny", "nz", "rv", "av", "ro", "ao", "hv", "ho"]
         for index, name in enumerate(lab_names):
-            Label(self.frame, text=name, width=8).grid(column=index, row=1)
-        self.frame.grid()
+            Label(self.data_f, text=name, width=8).grid(column=index, row=1)
+        self.frame.grid(row=0)
+        self.data_f.grid(row=1)
         self.draw_entry_boxes()
 
     def clear_entries(self):
@@ -126,11 +145,11 @@ class DataWindow(object):
         #Remove all old widgets from list
         self.data_entries[:] = []
         for row_no, data_row in enumerate(self.data_store):
-            lab = Label(self.frame, text="Galaxy no: %2u"% row_no)
+            lab = Label(self.data_f, text="Galaxy no: %2u"% row_no)
             lab.grid(row=row_no + 2, column = 0)
             entry_row = [lab]
             for col_no, data_piece in enumerate(data_row):
-                entry = Entry(master=self.frame, width=10)
+                entry = Entry(master=self.data_f, width=10)
                 entry.grid(row=row_no + 2, column=col_no + 1)
                 entry_row.append(entry)
                 entry.insert(0, data_piece)
